fab69555bc13937d7ae80e3d813c1c07eef0a0a9
Moeed Bhatti <moeedbhatti419@gmail.com>
2023-06-24 14:35:25 +0500

    delete vehicle done

--- src/Pages/HomePage/Body.jsx
+++ src/Pages/HomePage/Body.jsx
@@ -1,18 +1,106 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Container, Row, Col } from "reactstrap";
-import Card from "../../components/card/Card";
-// import 'bootstrap/dist/css/bootstrap.css'
 import axiosInstance from "../../helpers/axios";
-import { useSelector } from "react-redux";
-import Axios from "axios";
+import { Button, Table } from "antd";
+import { useCallback } from "react";
+import { PlusOutlined, DeleteFilled, EditFilled } from "@ant-design/icons";
+const defaultPageSize = 10;
 
 export default function () {
+  const deleteVehicle = useCallback(async (id) => {
+    try {
+      await axiosInstance.delete(`/vehicles?id=${id}`);
+      setVehicles((prev) => {
+        return prev.filter((vehicle) => vehicle._id !== id);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  const columns = [
+    {
+      title: "Model",
+      dataIndex: "model",
+      key: "model",
+    },
+    {
+      title: "Make",
+      dataIndex: "make",
+      key: "make",
+    },
+    {
+      title: "Color",
+      dataIndex: "color",
+      key: "color",
+    },
+    {
+      title: "Registered year",
+      dataIndex: "registeredYear",
+      key: "registeredYear",
+      sorter: {
+        compare: (a, b) => {
+          if (a.registeredYear < b.registeredYear) return -1;
+          if (b.registeredYear < a.registeredYear) return 1;
+          return 0;
+        },
+        multiple: 1,
+      },
+    },
+    {
+      title: "Vehicle Type",
+      dataIndex: "type",
+      key: "type",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (val) => (
+        <span
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Button
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            danger
+            onClick={async function () {
+              const isConfirm = confirm("Press a button!");
+              if (isConfirm === true) {
+                await deleteVehicle(val._id);
+              }
+            }}
+            icon={<DeleteFilled />}
+            type="primary"
+          ></Button>
+          <Button
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              background: "orange",
+            }}
+            icon={<EditFilled />}
+            type="primary"
+          ></Button>
+        </span>
+      ),
+    },
+  ];
   const token = localStorage.getItem("token") || null;
   const history = useHistory();
-  const [blogs, setBlogs] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
   const [count, setCount] = useState(null);
   const [loading, setLoading] = useState(true);
+  const getVehicles = useCallback(
+    async ({ limit, page }) =>
+      axiosInstance.get(`/vehicles?page=${page}&limit=${limit}`),
+    []
+  );
 
   useEffect(() => {
     if (!token) {
@@ -22,39 +110,80 @@ export default function () {
   useEffect(() => {
     (async () => {
       try {
-        const data = await axiosInstance.get("/vehicles/count");
+        const data = await Promise.all([
+          axiosInstance.get("/vehicles/count"),
+          getVehicles({ limit: defaultPageSize, page: 1 }),
+        ]);
         setCount(() => {
-          return data?.data?.count;
+          return data[0]?.data?.count;
         });
+        setVehicles(data[1]?.data?.data);
         setLoading(false);
       } catch (error) {
         console.log(error);
       }
     })();
   }, []);
-
   return (
-    <div>
-      <Container>
-        {count !== null && count !== undefined && (
-          <h1>Total Vehicles : {count} </h1>
-        )}
-        <Row>
-          {loading ? (
-            <h1>Loading....</h1>
-          ) : blogs.length === 0 ? (
-            <h1>No comment </h1>
-          ) : (
-            blogs.map((blog) => {
-              return (
-                <Col key={blog._id} md={4} sm={6} xs={12}>
-                  <Card name={`${blog.title}`} id={blog._id} cb={fetchPosts} />
-                </Col>
-              );
-            })
-          )}
-        </Row>
-      </Container>
-    </div>
+    <main
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Button
+        style={{
+          float: "right",
+          display: "flex",
+          color: "blue",
+          alignItems: "center",
+          justifyItems: "center",
+        }}
+        type="dashed"
+        icon={<PlusOutlined />}
+      >
+        Add
+      </Button>
+      {count !== null && count !== undefined && (
+        <h2
+          style={{
+            textAlign: "left",
+          }}
+        >
+          Total Vehicles : {count}{" "}
+        </h2>
+      )}
+      <Table
+        loading={loading}
+        pagination={{
+          defaultPageSize,
+          total: count,
+          onChange: async (page) => {
+            try {
+              setLoading(true);
+              const { data } = await getVehicles({
+                limit: defaultPageSize,
+                page,
+              });
+              setVehicles(data?.data);
+              setLoading(false);
+            } catch (error) {
+              console.log(error);
+            }
+          },
+        }}
+        style={{
+          maxWidth: "100%",
+          maxHeight: "100%",
+        }}
+        dataSource={vehicles.map((vehicle) => {
+          return {
+            key: vehicle._id,
+            ...vehicle,
+          };
+        })}
+        columns={columns}
+      />
+    </main>
   );
 }
